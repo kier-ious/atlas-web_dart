a24efb49337a66fcf523e108f7713de97f2f9e94
kier-ious <kier.diemer@gmail.com>
2024-10-08 13:20:02 -0500

    started on task 5, changed the function params to num instead of double in task 4

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -2,7 +2,7 @@ import '4-util.dart';
 import 'dart:convert';
 
 
-Future<double> calculateTotal() async {
+Future<num> calculateTotal() async {
   try {
     String userData = await fetchUserData();
     Map<String, dynamic> user = jsonDecode(userData);
