53fc112a2af07cf432b161fab23d9a123ffc1225
kier-ious <kier.diemer@gmail.com>
2024-10-08 12:59:10 -0500

    added several print statements and removed all my comments

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,22 +1,22 @@
 import '4-util.dart';
 import 'dart:convert';
 
-
 Future<double> calculateTotal() async {
   try {
     String userData = await fetchUserData();
     Map<String, dynamic> user = jsonDecode(userData);
     String userId = user['id'];
+    print('User ID: $userId');
 
     String ordersData = await fetchUserOrders(userId);
     List<dynamic> orders = jsonDecode(ordersData) ?? [];
+    print('Orders: $orders');
 
     if (orders.isEmpty) {
       print('No orders found for user: $userId');
       return -1;
     }
 
-    // Initializing start price
     double totalPrice = 0.0;
     for (var product in orders) {
       var productPriceData = await fetchProductPrice(product);
@@ -24,14 +24,19 @@ Future<double> calculateTotal() async {
         print('Failed to fetch price for product: $product');
         return -1;
       }
+
       var productPrice = jsonDecode(productPriceData);
       if (productPrice == null) {
         print('Decoded price null for product: $product');
         return -1;
       }
 
-      // Add to total price
-      totalPrice += productPrice;
+      if (productPrice is! num) {
+        print('Invalid product price type for product: $product, received: ${productPrice.runtimeType}');
+        return -1;
+      }
+
+      totalPrice += productPrice.toDouble();
     }
 
     return totalPrice;
